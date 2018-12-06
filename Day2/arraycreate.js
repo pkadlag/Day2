7079acaa227a989a7f273ef57e4c8816003814e5
pkadlag <45350880+pkadlag@users.noreply.github.com>
2018-12-06 18:20:21 +0530

    Delete arraycreate.js

--- Day2/arraycreate.js
+++
@@ -1,8 +0,0 @@
-var amitabh={name:"Amitabh" , surname:"Bachhan"};
-var abhishek=Object.create(amitabh);
-console.log(amitabh.name);
-console.log(amitabh.surname);
-console.log(abhishek.surname);
-console.log(__proto__);
-abhishek.name="Abhishek";
-console.log(abhishek.name);
