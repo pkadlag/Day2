012ab66c63b7caa65058c310727d0d03d95bc716
pkadlag <45350880+pkadlag@users.noreply.github.com>
2018-12-06 18:20:56 +0530

    Delete object.js

--- Day2/object.js
+++
@@ -1,7 +0,0 @@
-var person1={};
-var person2={name: "Punam"};
-var person3={name: "Punam" ,name2:"poonam",id: 101};
-console.log(person3);
-console.log(person3.name);
-console.log(person3["name"]);
-console.log(person3.id);
